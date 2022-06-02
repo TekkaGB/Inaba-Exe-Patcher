cbf9fd59e2e3b2d72d0e71508e5ae287d75e4353
AnimatedSwine37 <animatedswine37@gmail.com>
2022-06-02 18:39:57 +1000

    Ex patches now working

---
+++ ExPatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p4gpc.inaba
+{
+    public class ExPatch
+    {
+        /// <summary>
+        /// The name of the patch
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The pattern to sig scan for
+        /// </summary>
+        public string Pattern { get; set; }
+        /// <summary>
+        /// The function to add in the hook
+        /// </summary>
+        public string[] Function { get; set; }
+        /// <summary>
+        /// When to execute the function (first, after, or only)
+        /// </summary>
+        public string ExecutionOrder { get; set; }
+
+        public ExPatch(string name, string pattern, string[] function, string executionOrder)
+        {
+            Name = name;
+            Pattern = pattern;
+            Function = function;
+            ExecutionOrder = executionOrder;
+        }
+    }
+}
--- ExePatch.cs
+++ ExePatch.cs
@@ -8,6 +8,10 @@ using System.Linq;
 using p4gpc.inaba.Configuration;
 using System.Collections.Generic;
 using Reloaded.Memory.Sources;
+using System.Text.RegularExpressions;
+using System.Text;
+using Reloaded.Hooks.Definitions;
+using Reloaded.Hooks.Definitions.Enums;
 
 namespace p4gpc.inaba
 {
@@ -20,11 +24,13 @@ namespace p4gpc.inaba
         private readonly Process mProc;
         private readonly IntPtr mBaseAddr;
         private Scanner scanner;
+        private IReloadedHooks mHooks;
 
-        public ExePatch(ILogger logger, Config config)
+        public ExePatch(ILogger logger, Config config, IReloadedHooks hooks)
         {
             mLogger = logger;
             mConfig = config;
+            mHooks = hooks;
             mProc = Process.GetCurrentProcess();
             mBaseAddr = mProc.MainModule.BaseAddress;
             mem = new Memory();
@@ -71,9 +77,8 @@ namespace p4gpc.inaba
                 mLogger.WriteLine($"[Inaba Exe Patcher] (Debug) Replacement Content (in hex) = {BitConverter.ToString(fileContents).Replace("-", " ")}");
             }
 
-            var pattern = new CompiledScanPattern(BitConverter.ToString(fileHeader).Replace("-", " "));
-            var result = scanner.CompiledFindPattern(pattern, 0);
-
+            var result = scanner.FindPattern(BitConverter.ToString(fileHeader).Replace("-", " "));
+        
             if (result.Found)
             {
                 mem.SafeWriteRaw(mBaseAddr + result.Offset, fileContents);
@@ -114,23 +119,122 @@ namespace p4gpc.inaba
             // Load EnabledPatches in order
             foreach (string dir in patchPriorityList)
             {
-                string[] files;
+                string[] allFiles = Directory.GetFiles(dir, "*", SearchOption.TopDirectoryOnly);
+                    
+                string[] patches = allFiles.Where(s => Path.GetExtension(s).ToLower() == ".patch").ToArray();
+                string[] exPatches = allFiles.Where(s => Path.GetExtension(s).ToLower() == ".expatch").ToArray();
 
-                files = Directory.GetFiles(dir, "*", SearchOption.TopDirectoryOnly)
-                    .Where(s => (Path.GetExtension(s).ToLower() == ".patch")).ToArray();
-
-                if (files.Length == 0)
+                if (patches.Length == 0 && exPatches.Length == 0)
                 {
                     mLogger.WriteLine($"[Inaba Exe Patcher] No patches found in {dir}");
                     return;
                 }
 
                 mLogger.WriteLine($"[Inaba Exe Patcher] Found patches in {dir}");
-                foreach (string f in files)
+                foreach (string f in patches)
                 {
                     SinglePatch(f);
                 }
+
+                foreach(string f in exPatches)
+                {
+                    SinglePatchEx(f);
+                }
             }
+        }
+
+        private void SinglePatchEx(string filePath)
+        {
+            List<ExPatch> patches = ParseExPatch(filePath);
+            foreach(var patch in patches)
+            {
+                var result = scanner.FindPattern(patch.Pattern);
+                AsmHookBehaviour? order = null;
+                if (patch.ExecutionOrder == "before")
+                    order = AsmHookBehaviour.ExecuteFirst;
+                else if (patch.ExecutionOrder == "after")
+                    order = AsmHookBehaviour.ExecuteAfter;
+                else if (patch.ExecutionOrder == "only")
+                    order = AsmHookBehaviour.DoNotExecuteOriginal;
+                if(order != null)
+                    mHooks.CreateAsmHook(patch.Function, (long)(mBaseAddr + result.Offset), (AsmHookBehaviour)order).Activate();
+                else 
+                    mHooks.CreateAsmHook(patch.Function, (long)(mBaseAddr + result.Offset)).Activate();
+                mLogger.WriteLine($"[Inaba Exe Patcher] Applied patch {patch.Name} from {Path.GetFileName(filePath)}");
+            }
+        }
+
+        /// <summary>
+        /// Parses all of the patches from an expatch file
+        /// </summary>
+        /// <param name="filePath">The path to the expatch file</param>
+        /// <returns>A list of all of the found patches in the file</returns>
+        private List<ExPatch> ParseExPatch(string filePath)
+        {
+            List<ExPatch> patches = new List<ExPatch>();
+            bool startPatch = false;
+            List<string> currentPatch = new List<string>();
+            string patchName = "";
+            string pattern = "";
+            string order = "";
+
+            foreach (var line in File.ReadLines(filePath))
+            {
+                // Search for the start of a new patch (and its name)
+                var patchMatch = Regex.Match(line, @"\[\s*patch\s*(?:\s+(.*?))?\s*\]");
+                if (patchMatch.Success)
+                {
+                    startPatch = true;
+                    if (currentPatch.Count > 0)
+                    {
+                        patches.Add(new ExPatch(patchName, pattern, currentPatch.ToArray(), order));
+                    }
+                    currentPatch.Clear();
+                    if (patchMatch.Groups.Count > 1)
+                        patchName = RemoveComments(patchMatch.Groups[1].Value);
+                    else
+                        patchName = "";
+                    pattern = "";
+                    order = "";
+                    continue;
+                }
+
+                // Don't try to add stuff if the patch hasn't actually started yet
+                if (!startPatch) continue;
+
+                // Search for a patten to scan for
+                var patternMatch = Regex.Match(line, @"pattern\s*=\s*(.+)");
+                if (patternMatch.Success)
+                {
+                    pattern = RemoveComments(patternMatch.Groups[1].Value);
+                    continue;
+                }
+
+                // Search for an order to execute the hook in
+                var orderMatch = Regex.Match(line, @"\s*order\s*=\s*(.+)");
+                if (orderMatch.Success)
+                {
+                    order = RemoveComments(orderMatch.Groups[1].Value);
+                    continue;
+                }
+
+                // Add the line as a part of the patch's function
+                currentPatch.Add(RemoveComments(line));
+            }
+            if (currentPatch.Count > 0)
+            {
+                patches.Add(new ExPatch(patchName, pattern, currentPatch.ToArray(), order));
+            }
+            return patches;
+        }
+
+        /// <summary>
+        /// Searches for "//" in a string and removes it and anything after it
+        /// </summary>
+        /// <returns>A copy of the string with comments removed (or a copy of the string with no changes if there were no comments)</returns>
+        private string RemoveComments(string text)
+        {
+            return Regex.Replace(text, @"\/\/.*", "");
         }
 
         /// <inheritdoc />
